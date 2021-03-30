b0708b0edf0742afd09b1ef44bad606051378010
nocarryr <matt@nomadic-recording.com>
2021-03-30 09:10:56 -0500

    Move UHS data fixtures into conftest

---
+++ tests/conftest.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import json
+import pytest
+
+from tslumd import TallyColor, Message, Display
+
+HERE = Path(__file__).resolve().parent
+DATA_DIR = HERE / 'data'
+MESSAGE_FILE = DATA_DIR / 'uhs500-message.umd'
+MESSAGE_JSON = DATA_DIR / 'uhs500-tally.json'
+
+@pytest.fixture
+def uhs500_msg_bytes() -> bytes:
+    """Real message data received from an AV-UHS500 switcher
+    """
+    return MESSAGE_FILE.read_bytes()
+
+@pytest.fixture
+def uhs500_msg_parsed() -> Message:
+    """Expected :class:`~tslumd.messages.Message` object
+    matching data from :func:`uhs500_msg_bytes`
+    """
+    data = json.loads(MESSAGE_JSON.read_text())
+    data['scontrol'] = b''
+    displays = []
+    for disp in data['displays']:
+        for key in ['rh_tally', 'txt_tally', 'lh_tally']:
+            disp[key] = getattr(TallyColor, disp[key])
+        displays.append(Display(**disp))
+    data['displays'] = displays
+    return Message(**data)
