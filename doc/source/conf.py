0a68dba22a95e1cc8f2f10513d4e9aa23a6b4e92
nocarryr <matt@nomadic-recording.com>
2021-04-13 13:22:56 -0500

    Remove ugly red color from literals in doc builds

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -91,6 +91,10 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_css_files = [
+    'css/custom.css',
+]
+
 
 intersphinx_mapping = {
     'python':('https://docs.python.org/', None),
